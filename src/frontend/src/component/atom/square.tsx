55b5826b864f2f90a03ce8b5ecf3607d725b8c24
Kota <pontan0801.konta@gmail.com>
2024-01-10 15:11:26 +0000

    "Props名をわかりやすく変更しました"

--- src/frontend/src/component/atom/square.tsx
+++ src/frontend/src/component/atom/square.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { IconContext } from 'react-icons'
 import { MdSquare } from 'react-icons/md'
 
-type Props = {
+type SquareProps = {
 	color: string
 	size: string
 }
 
-const Square: React.FC<Props> = ({ color, size }) => {
+const Square: React.FC<SquareProps> = ({ color, size }) => {
 	return (
 		<IconContext.Provider value={{ color, size: size }}>
 			<MdSquare />
