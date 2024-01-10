d82134a38596f1f02c8eb4f0c62955b47c9e5878
Kota <pontan0801.konta@gmail.com>
2024-01-10 14:48:40 +0000

    build errorを解消しました

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import reportWebVitals from './utils/reportWebVitals'
+import RouteManager from './route/route'
 
 import './styles/style.sass'
-import RouteManager from './route/route'
 
 import '@fontsource/ibm-plex-sans-jp'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<RouteManager />)
 
-reportWebVitals()
+// reportWebVitals()
